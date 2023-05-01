68de08a105950c66764fb7b86283f9af9f49f401
takao-h <takao.hayashi@zozo.com>
2023-05-01 23:01:15 +0900

    Add test npm script

--- test-sample-project/test/example.test.ts
+++ test-sample-project/test/example.test.ts
@@ -1,5 +1,9 @@
-import{expect, test} from 'vitest'
+import{expect, it, test} from 'vitest'
 
 test('two plus two is four', () => {
-  expect(2+2).toBe(4)
+  expect(2 + 2).toBe(4)
+})
+
+it('one plus one is two', () => {
+  expect(1 + 1).toBe(2)
 })
